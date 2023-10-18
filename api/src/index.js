d55d80bf7beeda6ec130e74415642d9d86a3fe03
mohammed aziar <m.aziar@hotmail.com>
2023-10-18 14:39:57 +0100

    add variables environment into file docker-compose.yml andd call it into app nodejs

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const port = process.env.PORT;
+const host = process.env.HOST;
 const app = express();
 
 app.get('/test', (req, res) => {
     res.send("Our api server is working correctly")
 });
 
-app.listen(3000, () => {
-    console.log("Started api service");
+app.listen(port, () => {
+    console.log(`Started api service on port ${port}`);
+    console.log(`Our host is ${host}`);
 });
